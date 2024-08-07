eb875ff86c18a7dfabedbc0f399635fb045ef477
annguyen3112 <21021452@vnu.edu.vn>
2024-08-07 14:23:56 +0700

    Update signup_screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -2,6 +2,8 @@ import 'package:diabetes_app/screens/login_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:diabetes_app/database_helper.dart';
 
 class SignupScreen extends StatefulWidget {
   @override
@@ -9,16 +11,21 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
   bool passToggle = true;
   String? _selectedGender = 'Nam';
   DateTime? _selectedDate;
   final TextEditingController _dateController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   void dispose() {
     _dateController.dispose();
     _phoneController.dispose();
+    _nameController.dispose();
+    _passwordController.dispose();
     super.dispose();
   }
 
@@ -37,151 +44,195 @@ class _SignupScreenState extends State<SignupScreen> {
     }
   }
 
+  Future<void> _registerUser() async {
+    if (_formKey.currentState!.validate()) {
+      final user = {
+        'name': _nameController.text,
+        'phone': _phoneController.text,
+        'birthdate': _dateController.text,
+        'gender': _selectedGender,
+        'password': _passwordController.text,
+      };
+
+      await DatabaseHelper().insertUser(user);
+      Navigator.push(context, MaterialPageRoute(
+        builder: (context) => LoginScreen(),
+      ));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
         child: SafeArea(
-          child: Column(
-            children: [
-              SizedBox(height: 10),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Image.asset("assets/diabetes.png"),
-              ),
-              SizedBox(height: 15),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Họ và tên",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person),
-                  ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Image.asset("assets/diabetes.png"),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                child: TextField(
-                  controller: _phoneController,
-                  keyboardType: TextInputType.phone,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.digitsOnly,
-                    LengthLimitingTextInputFormatter(10),
-                  ],
-                  decoration: InputDecoration(
-                    labelText: "Số điện thoại",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.phone),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                child: TextField(
-                  controller: _dateController,
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    labelText: "Ngày sinh",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.date_range),
-                  ),
-                  onTap: () {
-                    _selectDate(context);
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(Icons.male_rounded),
-                        SizedBox(width: 12),
-                        Text(
-                          "Giới tính",
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ],
+                SizedBox(height: 15),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: "Họ và tên",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
                     ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: RadioListTile<String>(
-                            title: const Text('Nam'),
-                            value: 'Nam',
-                            groupValue: _selectedGender,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _selectedGender = value!;
-                              });
-                            },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Vui lòng nhập họ và tên';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextFormField(
+                    controller: _phoneController,
+                    keyboardType: TextInputType.phone,
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(10),
+                    ],
+                    decoration: InputDecoration(
+                      labelText: "Số điện thoại",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.phone),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Vui lòng nhập số điện thoại';
+                      } else if (value.length != 10) {
+                        return 'Số điện thoại phải là 10 số';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextFormField(
+                    controller: _dateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Ngày sinh",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.date_range),
+                    ),
+                    onTap: () {
+                      _selectDate(context);
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Vui lòng chọn ngày sinh';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.person),
+                          SizedBox(width: 12),
+                          Text(
+                            "Giới tính",
+                            style: TextStyle(fontSize: 16),
                           ),
-                        ),
-                        Expanded(
-                          child: RadioListTile<String>(
-                            title: const Text('Nữ'),
-                            value: 'Nữ',
-                            groupValue: _selectedGender,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _selectedGender = value!;
-                              });
-                            },
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: RadioListTile<String>(
+                              title: const Text('Nam'),
+                              value: 'Nam',
+                              groupValue: _selectedGender,
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _selectedGender = value!;
+                                });
+                              },
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
-                child: TextField(
-                  obscureText: passToggle ? true : false,
-                  decoration: InputDecoration(
-                    labelText: "Mật khẩu",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        setState(() {
-                          passToggle = !passToggle;
-                        });
-                      },
-                      child: passToggle
-                          ? Icon(CupertinoIcons.eye_slash_fill)
-                          : Icon(CupertinoIcons.eye_fill),
-                    ),
+                          Expanded(
+                            child: RadioListTile<String>(
+                              title: const Text('Nữ'),
+                              value: 'Nữ',
+                              groupValue: _selectedGender,
+                              onChanged: (String? value) {
+                                setState(() {
+                                  _selectedGender = value!;
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(10),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Material(
-                    color: Color(0xFF7165D6),
-                    borderRadius: BorderRadius.circular(10),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => LoginScreen(),
-                        ));
-                      },
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                        child: Center(
-                          child: Text(
-                            "Đăng ký",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold,
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: passToggle ? true : false,
+                    decoration: InputDecoration(
+                      labelText: "Mật khẩu",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          setState(() {
+                            passToggle = !passToggle;
+                          });
+                        },
+                        child: passToggle
+                            ? Icon(CupertinoIcons.eye_slash_fill)
+                            : Icon(CupertinoIcons.eye_fill),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Vui lòng nhập mật khẩu';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Material(
+                      color: Color(0xFF7165D6),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: _registerUser,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                          child: Center(
+                            child: Text(
+                              "Đăng ký",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
                         ),
@@ -189,36 +240,36 @@ class _SignupScreenState extends State<SignupScreen> {
                     ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Đã có tài khoản?",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.black54,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) => LoginScreen(),
-                      ));
-                    },
-                    child: Text(
-                      "Đăng nhập",
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Đã có tài khoản?",
                       style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF7165D6),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => LoginScreen(),
+                        ));
+                      },
+                      child: Text(
+                        "Đăng nhập",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF7165D6),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
