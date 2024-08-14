ab36631c1d615121a8d267fb28a3518c986ded92
annguyen3112 <21021452@vnu.edu.vn>
2024-08-14 10:17:10 +0700

    update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,16 +1,43 @@
+import 'package:diabetes_app/database.dart';
 import 'package:diabetes_app/screens/signup_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'home_screen.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen>{
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool passToggle = true;
+
+  Future<void> _loginUser() async {
+    final phone = _phoneController.text;
+    final password = _passwordController.text;
+
+    final user = await DatabaseHelper.instance.getUserByPhoneAndPassword(phone, password);
+
+    if (user != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen(userName: user['name'])),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Số điện thoại hoặc mật khẩu không đúng"),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child: SingleChildScrollView(
@@ -19,16 +46,22 @@ class _LoginScreenState extends State<LoginScreen>{
             children: [
               SizedBox(height: 10),
               Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Image.asset("assets/diabetes.png"),
+                padding: EdgeInsets.all(20),
+                child: Image.asset("assets/diabetes.png"),
               ),
               SizedBox(height: 10),
               Padding(
-                padding: EdgeInsets.all(12),
-                child: TextField(
+                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+                child: TextFormField(
+                  controller: _phoneController,
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    LengthLimitingTextInputFormatter(10),
+                  ],
                   decoration: InputDecoration(
+                    labelText: "Số điện thoại",
                     border: OutlineInputBorder(),
-                    label: Text("Nhập số điện thoại"),
                     prefixIcon: Icon(Icons.phone),
                   ),
                 ),
@@ -36,26 +69,19 @@ class _LoginScreenState extends State<LoginScreen>{
               Padding(
                 padding: EdgeInsets.all(12),
                 child: TextField(
-                  obscureText: passToggle ? true : false,
+                  controller: _passwordController,
+                  obscureText: passToggle,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
-                    label: Text("Nhập mật khẩu"),
+                    labelText: "Nhập mật khẩu",
                     prefixIcon: Icon(Icons.lock),
                     suffixIcon: InkWell(
-                      onTap: (){
-                        if (passToggle == true) {
-                          passToggle = false;
-                        }
-                        else {
-                          passToggle = true;
-                        }
+                      onTap: () {
                         setState(() {
-
+                          passToggle = !passToggle;
                         });
                       },
-                      child: passToggle
-                          ? Icon(CupertinoIcons.eye_slash_fill)
-                          : Icon(CupertinoIcons.eye_fill),
+                      child: Icon(passToggle ? CupertinoIcons.eye_slash_fill : CupertinoIcons.eye_fill),
                     ),
                   ),
                 ),
@@ -69,11 +95,7 @@ class _LoginScreenState extends State<LoginScreen>{
                     color: Color(0xFF7165D6),
                     borderRadius: BorderRadius.circular(10),
                     child: InkWell(
-                      onTap: (){
-                        // Navigator.push(context, MaterialPageRoute(
-                        //   builder: (context) => LoginScreen(),
-                        // ));
-                      },
+                      onTap: _loginUser,
                       child: Padding(
                         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
                         child: Center(
@@ -104,26 +126,25 @@ class _LoginScreenState extends State<LoginScreen>{
                     ),
                   ),
                   TextButton(
-                      onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => SignupScreen(),
-                        ));
-                      },
-                      child: Text(
-                          "Tạo tài khoản",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF7165D6),
-                          ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder:
+                          (context) => SignupScreen()));
+                    },
+                    child: Text(
+                      "Tạo tài khoản",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF7165D6),
                       ),
+                    ),
                   ),
                 ],
               ),
             ],
           ),
         ),
-      )
+      ),
     );
   }
 }
